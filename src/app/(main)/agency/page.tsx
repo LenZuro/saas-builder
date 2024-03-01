4b866c47775c687552e1ee55a3f8537ae497d348
LenZuro <126126939+LenZuro@users.noreply.github.com>
2024-03-01 11:48:08 -0800

    Started to set up stripe

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,14 +1,38 @@
 import { getAuthUserDetails, verifyAndAcceptInvite } from "@/lib/queries";
 import { currentUser } from "@clerk/nextjs";
+import { Plan } from "@prisma/client";
 import { redirect } from "next/navigation";
 import React from "react";
 
-const Page = async () => {
-  const authUser = await currentUser();
-  if (!authUser) return redirect("sign-in");
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: { plan: Plan; state: string; code: string };
+}) => {
   const agencyId = await verifyAndAcceptInvite();
+  console.log(agencyId);
   // Get user info
   const user = await getAuthUserDetails();
+  if (agencyId) {
+    if (user?.role === "SUBACCOUNT_GUEST" || user?.role === "SUBACCOUNT_USER") {
+      return redirect("/subaccount");
+    } else if (user?.role === "AGENCY_OWNER" || user?.role === "AGENCY_ADMIN") {
+      if (searchParams.plan) {
+        return redirect(
+          `/agency/${agencyId}/billing?plan=${searchParams.plan}`,
+        );
+      }
+      if (searchParams.state) {
+        const statePath = searchParams.state.split("__")[0];
+        const stateAgencyId = searchParams.state.split("___")[1];
+        if (!stateAgencyId) return <div>Not Authorized</div>;
+        return redirect(
+          `/agency/${stateAgencyId}/${statePath}?code=${searchParams.code}`,
+        );
+      }
+    }
+  }
+
   return <div>Agency</div>;
 };
 
